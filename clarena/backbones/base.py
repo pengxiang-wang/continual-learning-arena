fix-gradient-shap-baseline

clarena/backbones/base.py
@@ -634,19 +634,14 @@ class NISPAMaskBackbone(CLBackbone):
     ) -> None:
         r"""Register all `nn.Module`s of NISPA mechanism explicitly in this method. For `NISPAMaskBackbone`, they are the masks."""
 
-        self.candidate_stable_unit_mask_t: dict[str, dict[str, Tensor]] = {}
-
-        self.stable_unit_mask_t: dict[str, dict[str, Tensor]] = {}
-
-        self.plastic_unit_mask_t: dict[str, dict[str, Tensor]] = {}
-
-        self.weight_mask_t: dict[str, dict[str, Tensor]] = {}
-
-        self.bias_mask_t: dict[str, dict[str, Tensor]] = {}
-
-        self.frozen_weight_mask_t: dict[str, dict[str, Tensor]] = {}
-
-        self.frozen_bias_mask_t: dict[str, dict[str, Tensor]] = {}
+        self.weight_mask_t: dict[str, Tensor] = {}
+        r"""Store the weight mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has the same size (output features, input features) as weight."""
+        self.frozen_weight_mask_t: dict[str, Tensor] = {}
+        r"""Store the frozen weight mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has the same size (output features, input features) as weight."""
+        self.bias_mask_t: dict[str, Tensor] = {}
+        r"""Store the bias mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has the same size (output features, ) as bias. If the layer doesn't have bias, it is `None`."""
+        self.frozen_bias_mask_t: dict[str, Tensor] = {}
+        r"""Store the frozen bias mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has the same size (output features, ) as bias. If the layer doesn't have bias, it is `None`."""
 
         NISPAMaskBackbone.sanity_check(self)
 
@@ -654,13 +649,35 @@ class NISPAMaskBackbone(CLBackbone):
         r"""Check the sanity of the arguments."""
         pass
 
+    def initialise_parameter_mask(self) -> None:
+        r"""Initialise the parameter masks as zeros."""
+        for layer_name in self.weighted_layer_names:
+            layer = self.backbone.get_layer_by_name(
+                layer_name
+            )  # get the layer by its name
+
+            self.weight_mask_t[layer_name] = torch.zeros_like(layer.weight).to(
+                self.device
+            )
+            self.frozen_weight_mask_t[layer_name] = torch.zeros_like(layer.weight).to(
+                self.device
+            )
+
+            if layer.bias is not None:
+                self.bias_mask_t[layer_name] = torch.zeros_like(layer.bias).to(
+                    self.device
+                )
+                self.frozen_bias_mask_t[layer_name] = torch.zeros_like(layer.bias).to(
+                    self.device
+                )
+
     @override
     def forward(
         self,
         input: Tensor,
         stage: str,
     ) -> tuple[Tensor, dict[str, Tensor], dict[str, Tensor], dict[str, Tensor]]:
-        r"""The forward pass for data from task `task_id`. Task-specific mask for `task_id` are applied to the units in each layer.
+        r"""The forward pass for data from task `task_id`. Parameter mask is applied to the parameters in each layer.
 
         **Args:**
         - **input** (`Tensor`): The input tensor from data.
@@ -671,11 +688,7 @@ class NISPAMaskBackbone(CLBackbone):
 
         **Returns:**
         - **output_feature** (`Tensor`): the output feature tensor to be passed into heads. This is the main target of backpropagation.
-        - **weight_mask** (`dict[str, Tensor]`): the weight mask for the current task. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has same (output features, input features) as weight.
-        - **bias_mask** (`dict[str, Tensor]`): the bias mask for the current task. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has same (output features, ) as bias. If the layer doesn't have bias, it is `None`.
+        - **weight_mask** (`dict[str, Tensor]`): the weight mask. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has same (output features, input features) as weight.
+        - **bias_mask** (`dict[str, Tensor]`): the bias mask. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has same (output features, ) as bias. If the layer doesn't have bias, it is `None`.
         - **activations** (`dict[str, Tensor]`): the hidden features (after activation) in each weighted layer. Key (`str`) is the weighted layer name, value (`Tensor`) is the hidden feature tensor. This is used for the continual learning algorithms that need to use the hidden features for various purposes.
         """
-        # this should be copied to all subclasses. Make sure it is called to get the mask for the current task from the task embedding in this stage
-        weight_mask, bias_mask = self.get_mask(
-            stage,
-        )

clarena/cl_algorithms/fgadahat.py
@@ -25,7 +25,7 @@ from torch import Tensor
 
 from clarena.backbones import HATMaskBackbone
 from clarena.cl_algorithms import AdaHAT
-from clarena.cl_heads import HeadsCIL, HeadsTIL
+from clarena.cl_heads import HeadsTIL
 from clarena.utils import HATNetworkCapacity
 from clarena.utils.transforms import min_max_normalise
 
@@ -44,7 +44,7 @@ class FGAdaHAT(AdaHAT):
     def __init__(
         self,
         backbone: HATMaskBackbone,
-        heads: HeadsTIL | HeadsCIL,
+        heads: HeadsTIL,
         adjustment_intensity: float,
         importance_type: str,
         importance_summing_strategy: str,
@@ -69,7 +69,7 @@ class FGAdaHAT(AdaHAT):
 
         **Args:**
         - **backbone** (`HATMaskBackbone`): must be a backbone network with HAT mask mechanism.
-        - **heads** (`HeadsTIL` | `HeadsCIL`): output heads.
+        - **heads** (`HeadsTIL`): output heads. FG-AdaHAT algorithm only supports TIL (Task-Incremental Learning).
         - **adjustment_intensity** (`float`): hyperparameter, control the overall intensity of gradient adjustment. It is the $\alpha$ in the paper.
         - **importance_type** (`str`): the type of the neuron-wise importance, should be one of the following:
             1. 'input_weight_abs_sum':
@@ -1139,7 +1139,7 @@ class FGAdaHAT(AdaHAT):
         **Args:**
         - **layer_name** (`str`): the name of layer to get neuron-wise importance.
         - **input** (`Tensor` | `tuple[Tensor, ...]`): the input batch of the training step.
-        - **baselines** (`None` | `int` | `float` | `Tensor` | `tuple[int | float | Tensor, ...]`): starting point from which expectation is computed. Please refer to the [Captum documentation](https://captum.ai/api/layer.html#captum.attr.LayerGradientShap.attribute) for more details.
+        - **baselines** (`None` | `int` | `float` | `Tensor` | `tuple[int | float | Tensor, ...]`): starting point from which expectation is computed. Please refer to the [Captum documentation](https://captum.ai/api/layer.html#captum.attr.LayerGradientShap.attribute) for more details. If `None`, the baselines are set to zero.
         - **target** (`Tensor` | `None`): the target batch of the training step.
         - **batch_idx** (`int`): the index of the current batch. This is an argument of the forward function during training.
         - **num_batches** (`int`): the number of batches in the training step. This is an argument of the forward function during training.
@@ -1148,6 +1148,11 @@ class FGAdaHAT(AdaHAT):
         - **importance_step_layer** (`Tensor`): the neuron-wise importance of the layer of the training step.
         """
         layer = self.backbone.get_layer_by_name(layer_name)
+
+        if baselines is None:
+            baselines = torch.zeros_like(
+                input
+            )  # baselines are mandatory for GradientShap API. We explicitly set them to zero
 
         # initialise the Layer GradientShap object
         layer_gradientshap = LayerGradientShap(forward_func=self.forward, layer=layer)

clarena/cl_algorithms/hat.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 from clarena.backbones import HATMaskBackbone
 from clarena.cl_algorithms import CLAlgorithm
 from clarena.cl_algorithms.regularisers import HATMaskSparsityReg
-from clarena.cl_heads import HeadsCIL, HeadsTIL
+from clarena.cl_heads import HeadsTIL
 from clarena.utils import HATNetworkCapacity
 
 # always get logger for built-in logging in each module
@@ -30,7 +30,7 @@ class HAT(CLAlgorithm):
     def __init__(
         self,
         backbone: HATMaskBackbone,
-        heads: HeadsTIL | HeadsCIL,
+        heads: HeadsTIL,
         adjustment_mode: str,
         s_max: float,
         clamp_threshold: float,
@@ -43,7 +43,7 @@ class HAT(CLAlgorithm):
 
         **Args:**
         - **backbone** (`HATMaskBackbone`): must be a backbone network with HAT mask mechanism.
-        - **heads** (`HeadsTIL` | `HeadsCIL`): output heads.
+        - **heads** (`HeadsTIL`): output heads. HAT algorithm only supports TIL (Task-Incremental Learning).
         - **adjustment_mode** (`str`): the strategy of adjustment i.e. the mode of gradient clipping, should be one of the following:
             1. 'hat': set the gradients of parameters linking to masked units to zero. This is the way that HAT does, which fixes the part of network for previous tasks completely. See equation (2) in chapter 2.3 "Network Training" in [HAT paper](http://proceedings.mlr.press/v80/serra18a).
             2. 'hat_random': set the gradients of parameters linking to masked units to random 0-1 values. See the "Baselines" section in chapter 4.1 in [AdaHAT paper](https://link.springer.com/chapter/10.1007/978-3-031-70352-2_9).
@@ -89,7 +89,7 @@ class HAT(CLAlgorithm):
         self.masks: dict[str, dict[str, Tensor]] = {}
         r"""Store the binary attention mask of each previous task gated from the task embedding. Keys are task IDs (string type) and values are the corresponding mask. Each mask is a dict where keys are layer names and values are the binary mask tensor for the layer. The mask tensor has size (number of units, ). """
 
-        self.cumulative_mask_for_previous_tasks: dict[str, Tensor] = {}
+        self.cumulative_mask_for_previous_tasks: dict[str, dict[str, Tensor]] = {}
         r"""Store the cumulative binary attention mask $\mathrm{M}^{<t}$ of previous tasks $1,\cdots, t-1$, gated from the task embedding. Keys are task IDs and values are the corresponding cumulative mask. Each cumulative mask is a dict where keys are layer names and values are the binary mask tensor for the layer. The mask tensor has size (number of units, ). """
 
         # set manual optimisation

clarena/cl_algorithms/nispa.py
@@ -6,6 +6,7 @@ __all__ = ["NISPA"]
 
 import logging
 import math
+from copy import deepcopy
 from typing import Any
 
 import torch
@@ -14,7 +15,7 @@ from torch.utils.data import DataLoader
 
 from clarena.backbones import NISPAMaskBackbone
 from clarena.cl_algorithms import CLAlgorithm
-from clarena.cl_heads import HeadsCIL, HeadsTIL
+from clarena.cl_heads import HeadsTIL
 
 # always get logger for built-in logging in each module
 pylogger = logging.getLogger(__name__)
@@ -29,7 +30,7 @@ class NISPA(CLAlgorithm):
     def __init__(
         self,
         backbone: NISPAMaskBackbone,
-        heads: HeadsTIL | HeadsCIL,
+        heads: HeadsTIL,
         num_epochs_per_phase: int,
         accuracy_fall_threshold: float,
         k: float,
@@ -38,7 +39,7 @@ class NISPA(CLAlgorithm):
 
         **Args:**
         - **backbone** (`NISPAMaskBackbone`): must be a backbone network with NISPA mask mechanism.
-        - **heads** (`HeadsTIL` | `HeadsCIL`): output heads.
+        - **heads** (`HeadsTIL`): output heads. NISPA algorithm only supports TIL (Task-Incremental Learning).
         - **num_epochs_per_phase** (`int`): the number of epochs per phase. One phase consists of several epochs. At the end of each phase, unit selection and rewire is performed.
         - **accuracy_fall_threshold** (`float`): the accuracy fall threshold to stop the phases. If the accuracy of the current task is below this (best accuracy of phases - this threshold), the phase will stop, or else it will continue next phase.
         - **k** (`float`): hyperparameter for scheduling activation fraction $\tau$. See equation (3) in [NISPA paper](https://proceedings.mlr.press/v162/gurbuz22a/gurbuz22a.pdf).
@@ -48,16 +49,20 @@ class NISPA(CLAlgorithm):
 
         self.num_epochs_per_phase: int = num_epochs_per_phase
         r"""Store the number of epochs per phase."""
-
         self.accuracy_fall_threshold: float = accuracy_fall_threshold
         r"""Store the accuracy fall threshold to stop the phases."""
-
         self.k: float = k
         r"""Store the hyperparameter for scheduling activation fraction $\tau$. See equation (3) in [NISPA paper](https://proceedings.mlr.press/v162/gurbuz22a/gurbuz22a.pdf)."""
 
+        self.candidate_stable_unit_mask_t: dict[str, Tensor] = {}
+        r"""Store the candidate stable unit mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has size (number of units, )."""
+        self.stable_unit_mask_t: dict[str, Tensor] = {}
+        r"""Store the stable unit mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has size (number of units, )."""
+        self.plastic_unit_mask_t: dict[str, Tensor] = {}
+        r"""Store the plastic unit mask for each layer. Key (`str`) is layer name, value (`Tensor`) is the mask tensor. The mask tensor has size (number of units, )."""
+
         self.best_phase_acc: float
         r"""Store the best accuracy of the current task in the current phase."""
-
         self.phase_idx: int
         r"""Store the index of the current phase."""
 
@@ -69,12 +74,17 @@ class NISPA(CLAlgorithm):
 
         # initialise the masks at the beginning of first task. This should not be called in `__init__()` method as the `self.device` is not available at that time.
         if self.task_id == 1:
+
+            # initialise NISPA backbone weight mask
+            self.backbone.initialise_parameter_mask()
+
             for layer_name in self.backbone.weighted_layer_names:
                 layer = self.backbone.get_layer_by_name(
                     layer_name
                 )  # get the layer by its name
                 num_units = layer.weight.shape[0]
 
+                # initialise unit masks in NISPA algorithm
                 self.candidate_stable_unit_mask_t[layer_name] = torch.zeros(
                     num_units
                 ).to(self.device)
@@ -85,22 +95,8 @@ class NISPA(CLAlgorithm):
                 self.plastic_unit_mask_t[layer_name] = torch.ones(num_units).to(
                     self.device
                 )
-                self.weight_mask_t[layer_name] = torch.zeros_like(layer.weight).to(
-                    self.device
-                )
-                self.frozen_weight_mask_t[layer_name] = torch.zeros_like(
-                    layer.weight
-                ).to(self.device)
 
-                if layer.bias is not None:
-                    self.bias_mask_t[layer_name] = torch.zeros_like(layer.bias).to(
-                        self.device
-                    )
-                    self.frozen_bias_mask_t[layer_name] = torch.zeros_like(
-                        layer.bias
-                    ).to(self.device)
-
-    def clip_grad_by_mask(
+    def clip_grad_by_frozen_mask(
         self,
     ) -> None:
         r"""Clip the gradient by the frozen parameter mask. The gradient is multiplied by (1 - the frozen parameter mask) making masked parameters fixed. See "frozen connections" in [NISPA paper](https://proceedings.mlr.press/v162/gurbuz22a/gurbuz22a.pdf)."""
@@ -126,7 +122,7 @@ class NISPA(CLAlgorithm):
             1. 'train': training stage.
             2. 'validation': validation stage.
             3. 'test': testing stage.Applies only to training stage. For other stages, it is default `None`.
-        - **task_id** (`int`| `None`): the task ID where the data are from. If the stage is 'train' or 'validation', it should be the current task `self.task_id`. If stage is 'test', it could be from any seen task. In TIL, the task IDs of test data are provided thus this argument can be used. WSN algorithm works only for TIL.
+        - **task_id** (`int`| `None`): the task ID where the data are from. If the stage is 'train' or 'validation', it should be the current task `self.task_id`. If stage is 'test', it could be from any seen task. In TIL, the task IDs of test data are provided thus this argument can be used. NISPA algorithm works only for TIL.
 
         **Returns:**
         - **logits** (`Tensor`): the output logits tensor.
@@ -137,12 +133,6 @@ class NISPA(CLAlgorithm):
         feature, weight_mask, bias_mask, activations = self.backbone(
             input,
             stage=stage,
-            mask_percentage=self.mask_percentage,
-            test_mask=(
-                (self.weight_masks[f"{task_id}"], self.bias_masks[f"{task_id}"])
-                if stage == "test"
-                else None
-            ),
         )
         logits = self.heads(feature, task_id)
 
@@ -179,7 +169,7 @@ class NISPA(CLAlgorithm):
         # backward step (manually)
         self.manual_backward(loss)  # calculate the gradients
         # WSN hard clip gradients by the cumulative masks. See equation (4) in [WSN paper](https://proceedings.mlr.press/v162/kang22b/kang22b.pdf).
-        self.clip_grad_by_mask()
+        self.clip_grad_by_frozen_mask()
 
         # update parameters with the modified gradients
         opt.step()
@@ -211,7 +201,7 @@ class NISPA(CLAlgorithm):
         - **outputs** (`dict[str, Tensor]`): a dictionary contains loss and other metrics from this validation step. Key (`str`) is the metrics name, value (`Tensor`) is the metrics.
         """
         x, y = batch
-        logits, mask, activations = self.forward(
+        logits, weight_mask, bias_mask, activations = self.forward(
             x, stage="validation", task_id=self.task_id
         )
         loss_cls = self.criterion(logits, y)
@@ -232,6 +222,11 @@ class NISPA(CLAlgorithm):
 
         if self.current_epoch % self.num_epochs_per_phase == 0:
 
+            cached_state = {
+                "model": deepcopy(self.state_dict()),
+                "optimizer": self.trainer.optimizers[0].state_dict(),
+            }
+
             val_acc = outputs["acc"]  # accuracy of current epoch
             if val_acc > self.best_phase_acc:
                 self.best_phase_acc = val_acc  # update the best accuracy
@@ -244,28 +239,29 @@ class NISPA(CLAlgorithm):
                     1 + math.cos(self.phase_idx * math.pi / self.k)
                 )  # calculate the fraction of activation to select the candidate stable units. See equation (3) in [NISPA paper](https://proceedings.mlr.press/v162/gurbuz22a/gurbuz22a.pdf)
 
-                self.backbone.select_candidate_stable_units(
-                    train_dataloader=self.trainer.datamodule.train_dataloader(),
+                self.select_candidate_stable_units(
                     activation_fraction=tau,
                 )
 
+                # cached_stable_unit_mask = union(
+                #     deepcopy(self.stable_unit_mask_t), self.candidate_stable_unit_mask_t
+                # )
+
                 # rewire the connections
-                num_connections_dropped = (
-                    self.backbone.drop_connections_plastic_to_stable()
-                )
-                self.backbone.grow_new_connections(num_connections_dropped)
+                # num_connections_dropped = self.drop_connections_plastic_to_stable()
+                # self.grow_new_connections(num_connections_dropped)
 
-            # connection select and rewire
-            pass
+            else:
+                pass
 
-    def select_candidate_stable_units(
-        self, train_dataloader: DataLoader, activation_fraction: float
-    ) -> None:
+    def select_candidate_stable_units(self, activation_fraction: float) -> None:
         r"""Select candidate stable units that have highest summed activations in each layer. Thresholded by the fraction of summed activations of all units in the layer. See chapter 3.3 "Selecting Candidate Stable Units" in [NISPA paper](https://proceedings.mlr.press/v162/gurbuz22a/gurbuz22a.pdf).
 
         **Args:**
         - **activation_fraction** (`float`): the activation fraction threshold. See equation (3) in [NISPA paper](https://proceedings.mlr.press/v162/gurbuz22a/gurbuz22a.pdf). The value should be between 0 and 1.
         """
+        train_dataloader = self.trainer.datamodule.train_dataloader()
+
         summed_activations = {
             layer_name: torch.zeros_like(self.stable_unit_mask_t[layer_name])
             for layer_name in self.weighted_layer_names
@@ -286,7 +282,7 @@ class NISPA(CLAlgorithm):
                     summed_activations[layer_name] + activations[layer_name]
                 )
 
-        for layer_name in self.weighted_layer_names:
+        for layer_name in self.backbone.weighted_layer_names:
             summed_activations_layer = summed_activations[layer_name]
             summed_activations_layer_threshold = (
                 sum(summed_activations_layer) * activation_fraction
@@ -341,13 +337,11 @@ class NISPA(CLAlgorithm):
         **Returns:**
         - **outputs** (`dict[str, Tensor]`): a dictionary contains loss and other metrics from this test step. Key (`str`) is the metrics name, value (`Tensor`) is the metrics.
         """
-        test_task_id = self.get_test_task_id_from_dataloader_idx(dataloader_idx)
 
         x, y = batch
-        logits, mask, activations = self.forward(
+        logits, weight_mask, bias_mask, activations = self.forward(
             x,
             stage="test",
-            task_id=test_task_id,
         )  # use the corresponding head and mask to test (instead of the current task `self.task_id`)
         loss_cls = self.criterion(logits, y)
         acc = (logits.argmax(dim=1) == y).float().mean()

clarena/cl_algorithms/wsn.py
@@ -13,7 +13,7 @@ from torch.utils.data import DataLoader
 
 from clarena.backbones import WSNMaskBackbone
 from clarena.cl_algorithms import CLAlgorithm
-from clarena.cl_heads import HeadsCIL, HeadsTIL
+from clarena.cl_heads import HeadsTIL
 
 # always get logger for built-in logging in each module
 pylogger = logging.getLogger(__name__)
@@ -28,7 +28,7 @@ class WSN(CLAlgorithm):
     def __init__(
         self,
         backbone: WSNMaskBackbone,
-        heads: HeadsTIL | HeadsCIL,
+        heads: HeadsTIL,
         mask_percentage: float,
         parameter_score_init_mode: str = "default",
     ) -> None:
@@ -36,13 +36,12 @@ class WSN(CLAlgorithm):
 
         **Args:**
         - **backbone** (`WSNMaskBackbone`): must be a backbone network with WSN mask mechanism.
-        - **heads** (`HeadsTIL` | `HeadsCIL`): output heads.
+        - **heads** (`HeadsTIL`): output heads. WSN algorithm only supports TIL (Task-Incremental Learning).
         - **mask_percentage** (`float`): the percentage of parameters to be used for each task. See $c\%$ in [WSN paper](https://proceedings.mlr.press/v162/kang22b/kang22b.pdf).
         - **parameter_score_init_mode** (`str`): the initialisation mode for parameter scores, should be one of the following:
             1. 'default': the default initialisation mode in original WSN codes.
             2. 'N01': standard normal distribution $N(0, 1)$.
             3. 'U01': uniform distribution $U(0, 1)$.
-
         """
         CLAlgorithm.__init__(self, backbone=backbone, heads=heads)
 